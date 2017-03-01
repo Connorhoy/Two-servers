89bd5ed97b9818fc5b6b606110b951ccf349e3e5
Connor Hoy <connorhoy116@gmail.com>
2017-02-28 19:29:03 -0500

    Got it working.

--- twoservers.js
+++ twoservers.js
@@ -4,19 +4,27 @@
 var http = require("http");
 // Setups up both ports neccesarry for the application.
 var PORT = 7000;
-var PORT = 7500;
+var PORT2 = 7500;
 
-function handleRequest(request, response) {
-	if PORT = 7000 {
-		response.end("You are the greatest! " + request.url);
-	}
-	else {
-		response.end("You are not the greatest, or even good. In fact, you suck nerd.")
-	}
-}
 
 var server = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest2)
+
+
+
+function handleRequest(request, response) {
+	response.end("You are the greatest! " + request.url);
+}
+
+function handleRequest2(request, response) {
+	response.end("You are not the greatest, you are not even good. In fact, you are the worst. Suck it nerd. " + request.url);
+}
 
 server.listen(PORT, function() {
 	console.log("Server listening on: http://localhost:%s", PORT);
-})
+});
+
+
+server2.listen(PORT2, function() {
+	console.log("Server listening on: http://localhost:%s", PORT2);
+});
